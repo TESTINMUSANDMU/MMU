c34746211fd2c6081ba5fb71eae9253b6a4408e7
TESTINMUSANDMU <108117842+TESTINMUSANDMU@users.noreply.github.com>
2022-06-24 22:43:05 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -26,6 +26,7 @@ def start_pannel(_):
                 text=_["S_B_122"],
                 url=f"https://t.me/ov_tr",
             ),
+            [
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
