02b1eba396b8456c6694f027d539f0f2fe4edc91
TESTINMUSANDMU <108117842+TESTINMUSANDMU@users.noreply.github.com>
2022-06-24 13:26:08 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -135,4 +135,8 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons.append(
         [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
     )
+                    InlineKeyboardButton(
+                text=_["S_B_90"],
+                url=f"https://t.me/ov_tr",
+            ),
     return buttons
